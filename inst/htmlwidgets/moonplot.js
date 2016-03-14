72c631cb375a760985b9f2b93359fa22397ab814
Po Liu <po@NUMDOM2.numbers.net.au>
2016-03-14 17:44:49 +1100

    eod 1

--- inst/htmlwidgets/moonplot.js
+++ inst/htmlwidgets/moonplot.js
@@ -4,22 +4,15 @@ HTMLWidgets.widget({
 	
 	type: "output",
 	
-	factory: function(el, width, height) {
-		
-		// create our moonplot obj and bind it to the element
-		var mp = new moonplot(el.id);
-		
-		return {
-			renderValue: function(x) {
-				
-			},
-			
-			resize: function(width, height) {
-				
-			}
-			
-			m: mp
-		};
-		
+	initialize: function(el, width, height) {
+	  
+	},
+	
+	resize: function(el, width, height, instance) {
+	  
+	},
+	
+	renderValue: function(el, x, instance) {
+	  
 	}
 });
