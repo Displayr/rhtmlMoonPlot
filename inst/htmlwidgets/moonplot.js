43ecf502d1f785a2e2dd4bb33ceb78b4022bf31e
poliu2s <pliu0771@uni.sydney.edu.au>
2016-03-15 09:33:55 +1100

    drawing a circle

--- inst/htmlwidgets/moonplot.js
+++ inst/htmlwidgets/moonplot.js
@@ -13,6 +13,22 @@ HTMLWidgets.widget({
 	},
 	
 	renderValue: function(el, x, instance) {
+    var svgContainer = d3.select("body").append("svg")
+                                        .attr("width", 200)
+                                        .attr("height", 200);	
+    
+    
+    var textnode = document.createTextNode("water");    
+    //svgContainer.append("circle")
+    //            .attr("cx", 30)
+    //            .attr("cy", 30)
+    //            .attr("r", 20);
 	  
+	  el.id = svgContainer.append("circle")
+	                      .attr("cx", 30)
+	                      .attr("cy", 30)
+	                      .attr("r", 20);
+	                      
+	  el.appendChild(textnode);
 	}
 });
