94ba5e1f33cf49f18834d881a001d164c8e1e353
Po Liu <pliu0771@uni.sydney.edu.au>
2016-05-03 15:41:02 +1000

    another attempt at the policy

--- theSrc/R/rhtmlMoonPlot.R
+++ theSrc/R/rhtmlMoonPlot.R
@@ -30,8 +30,7 @@ moonplot <- function(
     width = width,
     height = height,
     sizingPolicy = htmlwidgets::sizingPolicy(
-      padding = 0,
-      browser.fill = TRUE
+      padding = 0
     ),
     package = 'rhtmlMoonPlot'
   )
