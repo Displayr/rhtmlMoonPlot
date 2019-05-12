640a157fa9320c27b059ada57281b0efdc63c70b
Kyle Zeeuwen <kyle.zeeuwen@gmail.com>
2019-05-13 08:45:37 +1000

    core link adjustment is working

--- theSrc/scripts/components/coreLabels.js
+++ theSrc/scripts/components/coreLabels.js
@@ -1,7 +1,7 @@
 import _ from 'lodash'
 import * as d3 from 'd3'
 import 'd3-transition'
-// import Utils from '../Utils'
+import {getLabelAnchorPoint} from '../labellers/coreLabeller'
 
 export class CoreLabels {
   constructor ({ parentContainer, plotState, cx, cy, fontFamily, fontSize, fontColor, fontSelectedColor, linkWidth, linkColor }) {
@@ -30,13 +30,17 @@ export class CoreLabels {
       .append('line')
       .attr('x1', d => d.anchor.x)
       .attr('y1', d => d.anchor.y)
-      .attr('x2', d => d.label.x)
-      .attr('y2', d => d.label.y)
+      // TODO labelLineConnector might be null, in which case we dont want to show the line,
+      // but I do want to create the line, in case the user drags the label and I need to show the line
+      // the current implementation of this behaviour is a little dodgy
+      .attr('x2', d => _.get(d, 'labelLineConnector.x',d.anchor.x))
+      .attr('y2', d => _.get(d, 'labelLineConnector.y',d.anchor.y))
       .attr('data-id', d => d.id)
       .attr('data-label', d => d.name)
       .attr('class', 'core-link')
       .attr('stroke-width', this.linkWidth)
       .attr('stroke', this.linkColor)
+      .attr('opacity', d => _.isNull(d.labelLineConnector) ? 0 : 1)
 
     this.labelSelection = this.parentContainer.selectAll('.core-label')
     this.labelSelection
@@ -78,15 +82,18 @@ export class CoreLabels {
 
     const dragEnd = function (d) {
       d3.select(this).style('fill', fontColor)
-      parentContainer.selectAll(`.core-link[data-id='${d.id}']`)
-        .attr('x2', d => d.label.x)
-        .attr('y2', d => d.label.y)
-        .attr('opacity', 1)
 
-      // TODO re-enable
-      // const coreLabels = labelSelection.nodes()
-      // Utils.adjustCoreLabelLength(coreLabels, plotState.getCircleRadius(), xCenter, yCenter)
-      // Utils.adjustCoreLinks(svg, coreLabels, anchorArray, linkWidth)
+      debugger
+      const allTheAnchors = _(plotState.getCoreLabels()).map('anchor').value()
+      const labelLineConnector = getLabelAnchorPoint(d.label, d.anchor, d.name, allTheAnchors)
+      d.labelLineConnector = labelLineConnector
+
+      parentContainer.selectAll(`.core-link[data-id='${d.id}']`)
+        .attr('x2', _.get(d, 'labelLineConnector.x',d.anchor.x))
+        .attr('y2', _.get(d, 'labelLineConnector.y',d.anchor.y))
+        .attr('opacity', _.isNull(d.labelLineConnector) ? 0 : 1)
+
+
       plotState.moveCoreLabel(d.id, d.label)
     }
 
--- theSrc/scripts/labellers/coreLabeller.js
+++ theSrc/scripts/labellers/coreLabeller.js
@@ -2,6 +2,16 @@ import labeler from './simulatedAnneallingLabellingAlgorithm'
 import {getLabelDimensionsUsingSvgApproximation} from '../labelUtils'
 import _ from 'lodash'
 
+const NONE = 'NONE'
+const BOTTOM_LEFT = 'BOTTOM_LEFT'
+const BOTTOM_CENTER = 'BOTTOM_CENTER'
+const BOTTOM_RIGHT = 'BOTTOM_RIGHT'
+const TOP_LEFT = 'TOP_LEFT'
+const TOP_CENTER = 'TOP_CENTER'
+const TOP_RIGHT = 'TOP_RIGHT'
+const MIDDLE_LEFT = 'MIDDLE_LEFT'
+const MIDDLE_RIGHT = 'MIDDLE_RIGHT'
+
 // ({plotState, coreLabelsData, svg, cx, cy, textColor, linkWidth})
 const positionLabels = ({
   svg,
@@ -28,7 +38,7 @@ const positionLabels = ({
         name: label.name,
         truncatedName: label.name,
         anchor: { x, y, r: 2 },
-        label: { x, y, width, height }, // NB putting width and height here for labeller
+        label: { x, y, width, height }, // NB putting width and height here for simulatedAnneallingLabellingAlgorithm and getLabelAnchorPoint
         width,
         height
       }
@@ -45,9 +55,90 @@ const positionLabels = ({
     .label(_(labels).map('label').value())
     .start(500)
 
-  _(labels).each(label => { label.label = _.pick(label.label, ['x', 'y']) })
+
+  const allTheAnchors = _(labels).map('anchor').value()
+  _(labels).each(label => { label.labelLineConnector = getLabelAnchorPoint(label.label, label.anchor, label.name, allTheAnchors) })
 
   return labels
 }
 
-module.exports = { positionLabels }
+const getLabelAnchorPoint = (lab, anc, name, allTheAnchors) => {
+  const labelLeft = lab.x - (lab.width / 2)
+  const labelRight = lab.x + (lab.width / 2)
+  const labelTop = (lab.y - lab.height)
+  const labelBottom = lab.y
+
+  const placementOptions = {
+    NONE:          null,
+    BOTTOM_LEFT:   { x: labelLeft,  y: lab.y},
+    BOTTOM_CENTER: { x: lab.x,      y: lab.y},
+    BOTTOM_RIGHT:  { x: labelRight, y: lab.y},
+    TOP_LEFT:      { x: labelLeft,  y: labelTop},
+    TOP_CENTER:    { x: lab.x,      y: labelTop},
+    TOP_RIGHT:     { x: labelRight, y: labelTop},
+    MIDDLE_LEFT:   { x: labelLeft,  y: lab.y - (lab.height / 2)},
+    MIDDLE_RIGHT:  { x: labelRight, y: lab.y - (lab.height / 2)}
+  }
+
+  const padding = 10
+  const horizontallyAligned = (anc.x > (labelLeft)) && (anc.x < (labelRight))
+  const anchorFarAbove = anc.y < (labelTop - padding)
+  const anchorAbove = anc.y < labelTop
+  const anchorFarBelow = anc.y > (labelBottom + padding)
+  const anchorBelow = anc.y > labelBottom
+  const anchorToLeft = anc.x < (labelLeft)
+  const anchorFarToLeft = anc.x < (labelLeft - padding)
+  const anchorToRight = anc.x > (labelRight)
+  const anchorFarToRight = anc.x > (labelRight + padding)
+
+  let placementOption = null
+  if      (horizontallyAligned && anchorFarAbove)   {placementOption = TOP_CENTER}
+  else if (horizontallyAligned && anchorFarBelow)   {placementOption = BOTTOM_CENTER}
+  else if (anchorAbove && anchorToLeft)             {placementOption = TOP_LEFT}
+  else if (anchorAbove && anchorToRight)            {placementOption = TOP_RIGHT}
+  else if (anchorBelow && anchorToLeft)             {placementOption = BOTTOM_LEFT}
+  else if (anchorBelow && anchorToRight)            {placementOption = BOTTOM_RIGHT}
+  else if (anchorFarToLeft)                         {placementOption = MIDDLE_LEFT}
+  else if (anchorFarToRight)                        {placementOption = MIDDLE_RIGHT}
+  else {
+    // Draw the link if there are any anc nearby
+    const ambiguityFactor = 10
+    const padL = placementOptions[TOP_LEFT]['x'] - ambiguityFactor
+    const padR = placementOptions[BOTTOM_RIGHT]['x'] + ambiguityFactor
+    const padT = placementOptions[TOP_LEFT]['y'] - ambiguityFactor
+    const padB = placementOptions[BOTTOM_RIGHT]['y'] + ambiguityFactor
+    let ancNearby = 0
+    for (anc of Array.from(allTheAnchors)) {
+      if (((anc.x > padL) && (anc.x < padR)) && ((anc.y > padT) && (anc.y < padB))) {
+        ancNearby++
+      }
+    }
+    if (ancNearby > 1) {
+      if (!anchorToLeft && !anchorToRight && !anchorAbove && !anchorBelow) {
+        placementOption = BOTTOM_CENTER
+      } else if (horizontallyAligned && anchorAbove) {
+        placementOption = TOP_CENTER
+      } else if (horizontallyAligned && anchorBelow) {
+        placementOption = BOTTOM_CENTER
+      } else if (anchorToLeft && anchorAbove) {
+        placementOption = TOP_LEFT
+      } else if (anchorToLeft && anchorBelow) {
+        placementOption = BOTTOM_LEFT
+      } else if (anchorToRight && anchorAbove) {
+        placementOption = TOP_RIGHT
+      } else if (anchorToRight && anchorBelow) {
+        placementOption = BOTTOM_RIGHT
+      } else if (anchorToLeft) {
+        placementOption = MIDDLE_LEFT
+      } else if (anchorToRight) {
+        placementOption = MIDDLE_RIGHT
+      }
+    } else {
+      placementOption = NONE // else just return the label x,y
+    }
+  }
+  console.log(`label ${name} chose ${placementOption}`)
+  return placementOptions[placementOption]
+}
+
+module.exports = { positionLabels, getLabelAnchorPoint }
