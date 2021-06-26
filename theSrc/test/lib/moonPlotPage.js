7d39f2e7e09c769ee7d9bdec221b4ef87963b189
Kyle Zeeuwen <kyle.zeeuwen@gmail.com>
2021-06-26 15:03:36 +1000

    VIS-923: use extracted dragThing and getCoord

--- theSrc/test/lib/moonPlotPage.js
+++ theSrc/test/lib/moonPlotPage.js
@@ -1,3 +1,5 @@
+const { snapshotTesting: { interactions: { dragThing, getCoords } } } = require('rhtmlBuildUtils')
+
 class MoonPlotPage {
   constructor (page) {
     this.page = page
@@ -8,38 +10,18 @@ class MoonPlotPage {
   circleSelector () { return '.moon-circle' }
   resetButtonSelector () { return '.plot-reset-button' }
 
-  // duplicated in rhtmlSankeyTree
-  async getCoords (selector) {
-    const element = await this.page.$(selector)
-    const rect = await this.page.evaluate(element => {
-      const { top, left, bottom, right } = element.getBoundingClientRect()
-      return { top, left, bottom, right }
-    }, element)
-    return rect
-  }
-
-  // duplicated in rhtmlSankeyTree
-  async dragThing (selector, xMovement, yMovement) {
-    const coords = await this.getCoords(selector)
-    const steps = Math.floor(Math.max(Math.abs(xMovement), Math.abs(yMovement)) / 2)
-    await this.page.hover(selector)
-    await this.page.mouse.down()
-    await this.page.mouse.move(parseFloat(coords.left + xMovement), parseFloat(coords.top + yMovement), { steps })
-    return this.page.mouse.up()
-  }
-
   async dragSurfaceLabel (labelId, xMovement, yMovement) {
     const selector = this.surfaceLabelSelector(labelId)
-    return this.dragThing(selector, xMovement, yMovement)
+    return dragThing(this.page, selector, xMovement, yMovement)
   }
 
   async dragCoreLabel (labelId, xMovement, yMovement) {
     const selector = this.coreLabelSelector(labelId)
-    return this.dragThing(selector, xMovement, yMovement)
+    return dragThing(this.page, selector, xMovement, yMovement)
   }
 
   async brittleCircleResize () {
-    const coords = await this.getCoords(this.circleSelector())
+    const coords = await getCoords(this.page, this.circleSelector())
     const radius = 166.67
     const offsetForTopOf500x500Circle = { x: coords.left + radius, y: coords.top }
     await this.page.mouse.move(offsetForTopOf500x500Circle.x, offsetForTopOf500x500Circle.y)
